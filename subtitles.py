848db761859a944476611ce33e12e7b316425fd8
Kanu Madhok <kanu.madhok@gmail.com>
2025-02-22 08:13:59 -0600

    Add files via upload

---
+++ subtitles.py
@@ -0,0 +1,20 @@
+import assemblyai as aai
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+#aai.settings.api_key = "8beccdeb004c4ba98617171e40ee7504"
+aai.settings.api_key = os.getenv('assembly_api_key')
+
+transcriber = aai.Transcriber()
+
+transcript = transcriber.transcribe("/Users/kanumadhok/Downloads/brainrot/Shorter_Speech.mp3")
+
+print(transcript.export_subtitles_srt())
+
+# print(transcript.export_subtitles_vtt())
+# Export SRT subtitles
+srt_subtitles = transcript.export_subtitles_srt()
+with open("/Users/kanumadhok/Downloads/brainrot/subtitles.srt", "w", encoding="utf-8") as srt_file:
+    srt_file.write(srt_subtitles)
